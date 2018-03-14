1bb1a8a72e920edd6c89c9447ee65d28eb023d4e
Tomas Jacik <tomas@jacik.cz>
2018-03-14 18:35:50 +0100

    Travis: removed code-checker testing

--- runTests.sh
+++ runTests.sh
@@ -8,15 +8,7 @@ if [ ! -d "php-parallel-lint" ]; then
     composer create-project --no-interaction jakub-onderka/php-parallel-lint php-parallel-lint ^0.9
 fi
 
-if [ ! -d "code-checker" ]; then
-    composer create-project --no-interaction nette/code-checker code-checker ^2.7
-fi
-
-php php-parallel-lint/parallel-lint.php -e php,phpt src
-php php-parallel-lint/parallel-lint.php -e php,phpt tests
-
-php code-checker/src/code-checker.php --short-arrays -d src
-php code-checker/src/code-checker.php --short-arrays -d tests
+php php-parallel-lint/parallel-lint.php -e php,phpt src tests
 
 vendor/bin/tester -s -p php5.6 -c tests/php.ini tests
 vendor/bin/tester -s -p php7.0 -c tests/php.ini tests
