0c763174510ac98655514a16409f4d4c0e31cf1b
Tomas Jacik <tomas@jacik.cz>
2018-03-14 18:38:43 +0100

    Travis: added PHP 7.2 support

--- runTests.sh
+++ runTests.sh
@@ -13,3 +13,4 @@ php php-parallel-lint/parallel-lint.php -e php,phpt src tests
 vendor/bin/tester -s -p php5.6 -c tests/php.ini tests
 vendor/bin/tester -s -p php7.0 -c tests/php.ini tests
 vendor/bin/tester -s -p php7.1 -c tests/php.ini tests
+vendor/bin/tester -s -p php7.2 -c tests/php.ini tests
